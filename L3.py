597600eaf31aac7f7842a282088a5c8b7592d645
ablopez02 <157068539+ablopez02@users.noreply.github.com>
2024-02-01 16:15:03 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,6 @@
+import math
+a=math.tan(math.pi/4)
+numerator=math.sqrt(a+1)
+denom=math.sqrt(math.tan(math.pi/3) +math.sin(math.pi/3))
+value=numerator/denom
+print(value)
---
+++ L4.py
@@ -0,0 +1,6 @@
+import math
+r=4
+area_c=math.pi*(4**2)
+area_s=1
+value= area_c-area_s
+print(value)
