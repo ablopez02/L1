b74f969b44d4c86de3b81da414ab9d05c4f18707
ablopez02 <157068539+ablopez02@users.noreply.github.com>
2024-02-01 16:10:07 -0500

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,16 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #the authors names are abbie and anastacia
+>>> x=3
+>>> print(x)
+3
+>>> y=x*5+x
+>>> print('Thecalculated amount is',y)
+Thecalculated amount is 18
+>>> print(int(False))
+0
+>>> print(float(True))
+1.0
+>>> z=1.2
+>>> print(int(z))
+1
